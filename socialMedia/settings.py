1b0cea04bc99d60e442c063073a5561d30feeccf
gabebonham <grotegabriel@gmail.com>
2025-02-11 22:40:34 -0300

    updated requirements

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'redesocialrimplesemdjango', 'display', 'static'),
+    os.path.join(BASE_DIR, 'display', 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
