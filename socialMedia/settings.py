07167af5cbf4fd829bc1d3c4443d2a4c58779a9b
gabebonham <grotegabriel@gmail.com>
2025-02-12 01:43:45 -0300

    changed to postgres

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'display', 'static'),
+    os.path.join(BASE_DIR, 'display', 'static', 'css'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
